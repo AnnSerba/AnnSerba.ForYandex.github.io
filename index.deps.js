43f6153ed06e5384cfd10f80bfe48c04ebc74b3d
Аня Серба <annserba94@gmail.com>
2016-07-10 15:15:12 +0300

    publication for Yandex

---
+++ index.deps.js
@@ -0,0 +1,107 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "mod": "theme"
+    },
+    {
+        "block": "page",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "ua",
+        "elem": "svg"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "content"
+    },
+    {
+        "block": "point"
+    },
+    {
+        "block": "image"
+    },
+    {
+        "block": "pages-points"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    }
+];
